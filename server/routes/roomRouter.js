62b7e607f6b59fe05644efbdaa7ca28c37d33ab1
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-01-20 21:07:08 -0600

    moongose

---
+++ server/routes/roomRouter.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { createRoom } from '../controllers/room.js';
+import auth from '../middleware/auth.js';
+
+const roomRouter = Router();
+
+roomRouter.post('/', auth,createRoom);
+
+export default roomRouter;
